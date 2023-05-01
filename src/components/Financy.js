cb5e4291789a53ca156cb60f4dcea43e1ba037fc
tushari789 <tushar789imran@gmail.com>
2023-05-01 23:45:06 +0600

    all components create done
    all components create done and primary color and text color added

---
+++ src/components/Financy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Financy = () => {
+  return (
+    <div>Financy</div>
+  )
+}
+
+export default Financy
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3734A9',
+        'secondary': '#FF7F5C',
+        'para': '#64607D'
+      },
+    },
+  },
+  plugins: [],
+}
+
